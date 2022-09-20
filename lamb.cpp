f6860db925faf0fe026502a505c0217235db059c
AnkitaK72 <110518319+AnkitaK72@users.noreply.github.com>
2022-09-20 11:27:38 +0530

    updated lamb.cpp

--- lamb.cpp
+++ lamb.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int main(){
     vector<int> a = {3, 5, 8, 1, 10};
     sort(a.begin(),a.end(),[](int a,int b){return a<b;});
-   // auto mylambda = [&](int result){
-  //      for(int i=0;i<a.size();i++){
-  //          a[i] +=result;
-   //     }
-   // };
+    auto mylambda = [&](int result){
+       for(int i=0;i<a.size();i++){
+            a[i] +=result;
+       }
+    };
 
     //mylambda(3);
    for(auto x : a){
